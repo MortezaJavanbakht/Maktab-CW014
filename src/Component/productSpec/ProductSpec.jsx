fb1c86676dfb7343575e3a85a458c1f988bd912a
Morteza Javanbakht <114153047+MortezaJavanbakht@users.noreply.github.com>
2023-01-12 16:08:52 +0330

    Complete modal

--- src/Component/productSpec/ProductSpec.jsx
+++ src/Component/productSpec/ProductSpec.jsx
@@ -1,16 +1,17 @@
 import "./ProductSpec.css"
-function PoductSpec({title, imgSrc, description, price}) {
-    return (<><div className="productSpecModal d-f-r j-c-c a-i-c g-24">
-        <img src={imgSrc} alt="product_image" />
+import Button from "../Button/Button";
+function PoductSpec({id, title, img, description, price}) {
+    return (<div className="modal"><div className="productSpecModal d-f-r j-c-c a-i-c g-24 bordered">
+        <img src={img} alt="product_image" className="product_image"/>
         <div className="productSpec__textPart d-f-c g-16">
             <h3>{title}</h3>
             <p>{description}</p>
-            <div className="productSpec__textPart__downPart">
+            <div className="productSpec__textPart__downPart d-f-r">
                 <p>Price: ${price}</p>
-                <button>Add to cart</button>
+                <Button name="Add to cart"/>
             </div>
             </div>
-    </div><div className="closeModal">x</div>  </>);
+    </div><div className="closeModal" onClick={()=>{document.querySelector(".modal").remove()}}>x</div>  </div>);
 }
 
 export default PoductSpec;
