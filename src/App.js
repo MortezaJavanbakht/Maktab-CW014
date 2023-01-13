e7ab988bf94f04806a745b986bf903c955e2891a
Ali-MHoseini <ali.mhoseini99@gmail.com>
2023-01-13 16:21:06 +0330

    Create Modal-Set Filters

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React ,{useState} from "react";
 import "./App.css";
 import Header from "./Component/Header/Header";
 import Navbar from "./Component/Navbar/Navbar";
-import Products from "./Component/Products/Products";
 import Footer from "./Component/Footer/Footer";
 import Form from "./Component/Form/Form";
 import CartItem from "./Component/CartItem/CartItem";
 import Button from "./Component/Button/Button";
+import Card from "./Component/Card/Card";
+import ProductSpec from "./Component/productSpec/ProductSpec";
+
+
+const productsArr = [
+    {id:1 , title:"Midi sundress with shirring detail" , img:"image/1.jpg",size:["S","L"] , price:29.5 , description:"This is for all the latest trends, no matter who you are, where you’re from and what you’re up to. Exclusive to ASOS, our universal brand is here for you, and comes in all our fit ranges: ASOS Curve, Tall, Petite and Maternity. Created by us, styled by you."},
+    {id:2 , title:"Midi sundress with ruched front" , img:"image/2.jpg",size:["XL","L"] , price:14 , description:"This is for all the latest trends, no matter who you are, where you’re from and what you’re up to. Exclusive to ASOS, our universal brand is here for you, and comes in all our fit ranges: ASOS Curve, Tall, Petite and Maternity. Created by us, styled by you."},
+    {id:3 , title:"Midi dress in pink ditsy floral" , img:"image/3.jpg",size:["M","L"] , price:25.8 , description:"This is for all the latest trends, no matter who you are, where you’re from and what you’re up to. Exclusive to ASOS, our universal brand is here for you, and comes in all our fit ranges: ASOS Curve, Tall, Petite and Maternity. Created by us, styled by you."},
+    {id:4 , title:"cami maxi dress in polka dot" , img:"image/4.jpg",size:["XL","L"] , price:17.5 , description:"This is for all the latest trends, no matter who you are, where you’re from and what you’re up to. Exclusive to ASOS, our universal brand is here for you, and comes in all our fit ranges: ASOS Curve, Tall, Petite and Maternity. Created by us, styled by you."},
+    {id:5 , title:"Frill mini dress in yellow polka dot" , img:"image/5.jpg",size:["XL","L"] , price:40 , description:"This is for all the latest trends, no matter who you are, where you’re from and what you’re up to. Exclusive to ASOS, our universal brand is here for you, and comes in all our fit ranges: ASOS Curve, Tall, Petite and Maternity. Created by us, styled by you."},
+    {id:6 , title:"Midi tea dress in blue and red spot" , img:"image/6.jpg",size:["XL","L"] , price:35, description:"This is for all the latest trends, no matter who you are, where you’re from and what you’re up to. Exclusive to ASOS, our universal brand is here for you, and comes in all our fit ranges: ASOS Curve, Tall, Petite and Maternity. Created by us, styled by you."},
+]
 
 function App() {
-  return (
+    const [products,setProducts] = useState(productsArr)
+    return (
     <>
       <Header />
       <div className="mainContainer">
         <div className="left-side">
-          <Navbar />
-          <Products />
+          <Navbar list={productsArr} SetList={setProducts} />
+            <div className='card-container'>
+                {products.map(item=>
+                    <Card
+                        key={item.id}
+                        img={item.img}
+                        title={item.title}
+                        price={item.price}
+                        description={item.description} />
+
+                )}
+            </div>
         </div>
         <div className="right-side">
             <div className='cart-Box'>
--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./Card.css"
 import Button from '../Button/Button'
+import ProductSpec from "../productSpec/ProductSpec";
 
-export default function Card({title,img,price}) {
+export default function Card({title,img,price,description}) {
+    const [showModal,setShowModal] = useState(false);
   return (
     <div className='main-card'>
-        <img src={img} alt="" />
+        <img src={img} alt="" onClick={()=>
+        {
+            setShowModal(true)
+
+        }
+        }/>
         <h2 className='title'>{title}</h2>
         <div className='price-add'>
         <span className='price'>${price}</span>
         <Button name="Add To Cart" />
+            <ProductSpec title={title} img={img} price={price} description={description} Show={showModal}
+                         Func={()=>setShowModal(false)}/>
         </div>
+
     </div>
   )
 }
