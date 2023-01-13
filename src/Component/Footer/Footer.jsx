a0cea14db619e97bba15cb6c0abe9a52942eee66
unknown <nima.soltan2525@gmail.com>
2023-01-13 11:06:29 -0800

    merge

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Footer.css"
+export default function Footer(){
+    return(
+        <div className='FooterMain'>
+            <div><p className='text'>All right is reserved</p></div>
+        </div>
+    )
+}
